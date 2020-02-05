5938f661d914c6046c04b84a4869f3f8e3846b19
taoki <taoki@askul.com>
2020-02-05 09:14:50 +0900

    fib関数の実装

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
 'use strict';
+function fib(n) {
+    if (n === 0) {
+        return 0;
+    }
+    if (n === 1) {
+        return 1;
+    }
+    if (n >= 2) {
+        return fib(n - 1) + fib(n - 2);
+    }
+    return null;
+}
+const length = 40;
+for (let i = 0; i <= length; i++) {
+  console.log(fib(i));
+}
